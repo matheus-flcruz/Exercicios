709954c4fdef1885333d8951f92b813d0f0c9dff
Matheus <matheus.flcruz@gmail.com>
2025-06-26 15:33:32 -0300

    Resolução dos exercícios do 5 ao 10.

--- DIO/JS/exercicios.js
+++ DIO/JS/exercicios.js
@@ -59,3 +59,133 @@ function multiploDeTresECinco(entrada2) {
 }
 
 console.log(multiploDeTresECinco(entrada2));
+
+// 6 ----------------------------------------------------------------------
+
+const matheus = {
+    nome: 'Matheus',
+    idade: 35,
+}
+
+const dandara = {
+    nome: 'Dandara',
+    idade: 4,
+}
+
+const maria = {
+    nome: 'Maria',
+    idade: 68,
+}
+
+const roberto = {
+    nome: 'Roberto',
+    idade: -1,
+}
+
+function maiorMenorIdade(pessoa) {
+    if (pessoa.idade >= 18 && pessoa.idade < 60) {
+        return `${pessoa.nome} é maior de idade.`;
+    } else if (pessoa.idade < 18 && pessoa.idade >= 0) {
+        return `${pessoa.nome} é menor de idade.`;
+    } else if (pessoa.idade >= 60) {
+        return `${pessoa.nome} é idoso(a).`;
+    } else {
+        return `${pessoa.nome} tem uma idade inválida.`;
+    }
+}
+
+console.log(maiorMenorIdade(matheus));
+console.log(maiorMenorIdade(dandara));
+console.log(maiorMenorIdade(maria));
+console.log(maiorMenorIdade(roberto));
+
+// 7 ----------------------------------------------------------------------
+
+let diaDaSemana = 5;
+
+switch (diaDaSemana) {
+    case 1:
+        console.log('Hoje é Domingo');
+        break;
+    case 2:
+        console.log('Hoje é Segunda-feira');
+        break;
+    case 3:
+        console.log('Hoje é Terça-feira');
+        break;
+    case 4:
+        console.log('Hoje é Quarta-feira');
+        break;
+    case 5:
+        console.log('Hoje é Quinta-feira');
+        break;
+    case 6:
+        console.log('Hoje é Sexta-feira');
+        break;
+    case 7:
+        console.log('Hoje é Sábado');
+        break;
+    default:
+        console.log('Dia inválido.');
+}
+
+// 8 ----------------------------------------------------------------------
+
+let entrada3 = 15;
+
+function calcularDobro(entrada3) {
+    return entrada3 * 2;
+}
+
+console.log(`O dobro de ${entrada3} é ${calcularDobro(entrada3)}.`);
+
+// 9 ----------------------------------------------------------------------
+
+const entrada4 = [10, 36];
+
+function maiorNumero(entrada4) {
+    if (entrada4[0] > entrada4[1]) {
+        return `O maior número é ${entrada4[0]}.`;
+    } else if (entrada4[0] < entrada4[1]) {
+        return `O maior número é ${entrada4[1]}.`;
+    } else {
+        return 'Os números são iguais.';
+    }
+}
+
+console.log(maiorNumero(entrada4));
+
+// 10 ---------------------------------------------------------------------
+
+const entrada5 = 10;
+
+function calcularFatorial(entrada5) {
+    if (entrada5 < 0) {
+        return 'Número inválido para fatorial.';
+    }
+    let fatorial = 1;
+    for (let i = 1; i <= entrada5; i++) {
+        fatorial *= i;
+    }
+    return `O fatorial de ${entrada5} é ${fatorial}.`;
+}
+
+console.log(calcularFatorial(entrada5));
+
+// 11 ---------------------------------------------------------------------
+
+
+
+// 12 ----------------------------------------------------------------------
+
+
+
+// 13 ----------------------------------------------------------------------
+
+
+
+// 14 ----------------------------------------------------------------------
+
+
+
+// 15 ----------------------------------------------------------------------
